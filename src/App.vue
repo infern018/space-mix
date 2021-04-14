2a4a35e1ca1b98ba985dbbcc2c673589c5fba193
Vishwas Singh <singh.vishu234@gmail.com>
2021-04-14 14:01:44 +0530

    track and validate user choices

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <h1>Flip 'em!</h1>
   <section class="game-board">
-    <Card v-for="(card, index) in cardList" :key="`card-${index}`" :value="card.value" :visible="card.visible" :position="card.position" @select-card="flipCard"/>
+    <Card v-for="(card, index) in cardList" :key="`card-${index}`" :value="card.value" :visible="card.visible" :position="card.position" :matched="card.matched" @select-card="flipCard"/>
   </section>
+  <h2> {{ status }} </h2>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import Card from './components/Card'
 
 export default {
@@ -14,22 +15,55 @@ export default {
   components:{Card},
   setup(){
     const cardList = ref([])
+    const userSelection = ref([])
+    const status = ref('')
 
     for(let i=0; i<16; i++){
       cardList.value.push({
         value:i,
         visible:false,
-        position:i
+        position:i,
+        matched:false
       })
     }
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
+
+      if(userSelection.value[0]){
+        userSelection.value[1] = payload
+      } else {
+        userSelection.value[0] = payload
+      }
     }
+
+    watch(userSelection, currentValue => {
+      if(currentValue.length === 2){
+        const cardOne = currentValue[0]
+        const cardTwo = currentValue[1]
+
+        if(cardOne.faceValue === cardTwo.faceValue){
+          status.value = "Match"
+          cardList.value[cardOne.position].matched = true;
+          cardList.value[cardTwo.position].matched = true;
+        } else {
+          status.value = "Mismatch"
+          cardList.value[cardOne.position].visible = false;
+          cardList.value[cardTwo.position].visible = false;
+
+        }
+
+        
+        userSelection.value.length = 0
+      } 
+    },
+    { deep: true })
 
     return{
       cardList,
-      flipCard
+      flipCard,
+      userSelection,
+      status
     }
   }
 }
